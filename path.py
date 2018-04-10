df21b4f80a5883ca4104a6a88b8b9fbb1f2b2c58
kasramvd <kasraavand@gmail.com>
2018-04-10 13:41:34 +0430

    first complete implementation of Dijkstra's algoritm for shortest path

--- path.py
+++ path.py
@@ -1,4 +1,6 @@
 from operator import attrgetter
+from functools import lru_cache
+from collections import deque
 
 
 class ShortestPath:
@@ -8,14 +10,31 @@ class ShortestPath:
         self.graph = args[0]
         __start = kwargs['start']
         self.sart = self.graph[__start]
-        __end = kwargs['end'] 
+        __end = kwargs['end']
         self.end = self.graph[__end]
+        self.dist_to = {}
+        self.edge_to = {}
 
-    def create_tree(self):
-        to_node = {}
-        while True:
-            start_neighbors = self.start.neighbors
-            to_node = {edge.right.name: edge.weight for edge in edges}
-            for edge in edges:
-                for n in node.neighbors:
-                    to_node[n.name] = to_node[node.name]
+    def shortest_path(self, start, end):
+        for v in self.graph.nodes:
+            self.dist_to[v] = float('inf')
+        self.dist_to[start] = 0.0
+        queue = deque()
+        queue.extendleft((0, start))
+        while queue:
+            self.relax(queue, queue.pop())
+
+    def relax(self, queue, node):
+        for n in node.neighbors:
+            # edge = self.graph.get_edge(node, n)
+            edge = self.graph.get_edge(node, n)
+            if self.dist_to[n] > self.dist_to[node] + edge.weight:
+                self.dist_to[n] = self.dist_to[node] + edge.weight;
+                self.edge_to[n] = edge;
+                ind = queue.index(n)
+                queue.insert(ind, (n, self.dist_to[n]))
+
+    def create_sp_tree(self):
+        # Create the shortest path tree
+        # for node in self.graph.node:
+        pass
