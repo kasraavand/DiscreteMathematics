e39f4fd56f334dc7689dc364d5a323622c74a219
kasramvd <kasraavand@gmail.com>
2018-04-15 18:22:52 +0430

    bug fix and improve performance

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 from graphs.graph import Graph
+from graphs.path import ShortestPath
 import random
-graph = Graph()
-lowers = "abcdefghijklmnopqrstuvwxyz"
-adj = set(random.sample(lowers, random.randint(0, 7)))
-for name in lowers:
-	graph.add_node(name, 0, **{a:[0, 1] for a in adj - {name}})
 
-print(graph.nodes)
-print(graph.edges)
+
+graph = Graph()
+lowers = "abcdefgh"
+for name in lowers:
+	adj = set(random.sample(lowers, random.randint(0, 7)))
+	ne = {a:[round(random.random()*10, 0), 1] for a in adj - {name}}
+	graph.add_node(name, 0, **ne)
+
+# print(graph.nodes)
+# print(graph.edges)
+
+S = ShortestPath(graph)
+
+S.shortest_path('a', 'e')
+
+print(graph)
+print(S.edge_to)
